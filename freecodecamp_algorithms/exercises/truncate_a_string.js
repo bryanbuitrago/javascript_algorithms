8c33392b73ee2a6fa083b3783313aa552d31284c
bryan buitrago <bryanbuitrago86@gmail.com>
2017-08-04 12:49:17 -0400

    Truncate a string algorithm finished

--- freecodecamp_algorithms/exercises/truncate_a_string.js
+++ freecodecamp_algorithms/exercises/truncate_a_string.js
@@ -1,26 +1,42 @@
+// ======== Truncate a string ==========
+// Truncate a string (first argument) if it is longer than the given maximum
+// string length (second argument). Return the truncated string with a ... ending.
+
+// Note that inserting the three dots to the end will add to the string length.
+// However, if the given maximum string length num is less than or equal to 3, then
+// the addition of the three dots does not add to the string length in determining
+// the truncated string.
 
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
+  // truncate the string
 
-  // add ... to the str if exceeds the num of characters without the str length > num
-  // turn str into an array
-  str = str.split('');
-  // loop over array
-  for(var i = 0; i < str.length; i++) {
+  // if (num <= 3) remove chars after the indicated num,
+  // add 3 dots to the str without adding to the length
+  // else if (str.length > num)
 
-    if(i >= num) {
-      str.splice(i);
-    } if(str.length === num) {
-      str.splice(-3);
-    }
+  // substract 3 from the num (the 3 ... added to the end)
+  // remove all characters after
+  // add 3 dots to the str without adding to the length
+
+  // else return the str
+
+  let dots = '...';
+
+  else if(str.length > num) {
+  if(num <= 3) {
+   str = str.slice(0, num);
+   str = str.concat(dots);
+  } else if(str.length > num) {
+    num -= 3;
+    str = str.slice(0, num);
+    str = str.concat(dots);
   }
-  while(str.length < num) {
-    str.push('.');
-  }
-  // find length of the str
-  // if str.length > num
-//   console.log(str.length);
-  return str.join('');
+  return str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+truncateString("A-tisket a-tasket A green and yellow basket", 11) //should return "A-tisket...".
+// truncateString("Peter Piper picked a peck of pickled peppers", 14) //should return "Peter Piper...".
+// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)// should return "A-tisket a-tasket A green and yellow basket".
+// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2)// should return "A-tisket a-tasket A green and yellow basket".
+// truncateString("A-", 1) // should return "A...".
+// truncateString("Absolutely Longer", 2) //should return "Ab...".
