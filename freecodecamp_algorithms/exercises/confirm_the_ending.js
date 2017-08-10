4e92a50384133566a789ed9df5b61c1bcbcc34ea
bryan buitrago <bryanbuitrago86@gmail.com>
2017-08-10 14:35:32 -0400

    confirm the ending algorithm completed

--- freecodecamp_algorithms/exercises/confirm_the_ending.js
+++ freecodecamp_algorithms/exercises/confirm_the_ending.js
@@ -1,40 +1,31 @@
-// ========== Confirm the Ending ==========
-// Check if a string (first argument, str) ends with the given target string (second argument,
-// target).
-
-// This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
-// But for the purpose of this challenge,
-// we would like you to use one of the JavaScript substring methods instead.
-
+// ========= Confirm the Ending =========
+// Check if a string (first argument, str) ends with the given target string
+// (second argument, target).
 
 function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
+  // check if a string  ends with the target string
 
-  // check if the last letter in a str matches the target letter or word
-  // turn str into an array
-  str = str.split(' ');
-  // // if str last letter or word === target return true
+  let i;
+  let strLength = str.length ;
+  let targetLength = target.length;
+  let strToMatch = '';
+  // loop over the str backward
+  // use the target as the length of characters the loop will go over
+  for(i = strLength - 1; i > (strLength - (targetLength + 1)); i--) {
+    strToMatch += str[i];
+    console.log(strToMatch);
+  }
 
-  // if else statement way
-  // if(str[str.length -1].includes(target)) {
-  //   return true;
-  // }
-  // // else return false
-  // return false;
-
-  // ternary operator way
-  // return str[str.length - 1].includes(target) ? true : false;
-
-  // .includes() method returns a boolean
-  return str[str.length - 1].includes(target);
+  // reverse str to match the str to target
+  // if there is a match return true
+  // else return false
+    return strToMatch.split('').reverse().join('') === target;
 }
 
 confirmEnding("Bastian", "n") //should return true.
-confirmEnding("Connor", "n") // should return false.
-confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") // should return false.
-confirmEnding("He has to give me a new name", "name") // should return true.
-confirmEnding("Open sesame", "same") // should return true.
-confirmEnding("Open sesame", "pen") // should return false.
-confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") // should return false.
-// Do not use the built-in method .endsWith() to solve the challenge.
+// confirmEnding("Connor", "n") //should return false.
+// confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") //should return false.
+// confirmEnding("He has to give me a new name", "name") //should return true.
+// confirmEnding("Open sesame", "same") //should return true.
+// confirmEnding("Open sesame", "pen") //should return false.
+// confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") //should return false.
